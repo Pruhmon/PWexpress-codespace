0bdf30191a1572035c0a161cd3b132bd1e7ddde6
Praman <Pruhmon@gmail.com>
2025-06-15 22:58:34 +0000

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 //create instance of Express app
 const app = express();
-
+const data = require("./test.json");
 app.set("view engine", "ejs");
 app.use(express.static("public")); 
 //index/home URL
@@ -27,7 +27,23 @@ app.get('/obama',(req,res)=>{
   res.render("pages/obama", {title:"obama"});
 });
 
+// Users list page
+app.get("/users", (req, res) => {
+  res.render("users/index", {
+    title: "Users",
+    users: data,
+  });
+});
 
+// User view page
+app.get("/users/view/:id", (req, res) => {
+  const user = data.find(u => u.id == req.params.id);
+  res.render("users/view", {
+    title: "User Page",
+    user,
+    bio_data: user.bio_data,
+  });
+});
 //Set server to listen for requests
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`);
